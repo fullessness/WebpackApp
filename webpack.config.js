96d98f57d96e1a63a65f52aab6c44179c6f8aaf1
fullessness <fullessness@users.noreply.github.com>
2018-05-20 15:52:45 +0200

    fix webpack plugins injection

--- webpack.config.js
+++ webpack.config.js
@@ -3,32 +3,28 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = env => {
-  module.exports.plugins = [{
-    title: 'WebpackApp for tutorial',
-    template: './src/index.html',
-    "hash": false,
-    "inject": 'head',
-    "inject": 'body',
-    "compile": true,
-    "favicon": false,
-    "minify": false,
-    "cache": true,
-    "showErrors": true,
-    "chunks": ['app', 'hello'], // "all"
-    "excludeChunks": []
-  }];
+  const _plugins = [
+    new HtmlWebpackPlugin({
+      title: 'WebpackApp for tutorial',
+      template: './src/index.html',
+      "hash": false,
+      "inject": 'head',
+      "inject": 'body',
+      "compile": true,
+      "favicon": false,
+      "minify": false,
+      "cache": true,
+      "showErrors": true,
+      "chunks": ['app', 'hello'], // "all"
+      "excludeChunks": []
+    })
+  ];
 
-  /* 
-    'env' variable is specified on scripts in package.json
-  */
   if (env != null && env.runCleanWebpackPlugin != null && env.runCleanWebpackPlugin === true) {
-    module.exports.plugins.push(
-      // empty 'dist' folder when run script
-      new CleanWebpackPlugin(['dist'])
-    );
+    _plugins.push(new CleanWebpackPlugin(['dist']));
   }
 
-  return {
+  const res = {
     entry: {
       app: './src/index.js',
       hello: './src/hello.ts',
@@ -47,6 +43,7 @@ module.exports = env => {
         }
       ]
     },
+    plugins: _plugins,
     devServer: {
       contentBase: './dist'
     },
@@ -54,5 +51,7 @@ module.exports = env => {
       extensions: ['.tsx', '.ts', '.js']
     }
   };
+
+  return res;
 }
 
